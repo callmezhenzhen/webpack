6abab5b988e79583f69a301569cb66e3f1ba67bf
ddz10349 <ddz10349@ly.com>
2019-11-26 08:54:06 +0800

    webpack-dev-server

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,10 +4,21 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     // 用于解析entry和module.rules.loader选项
     context: path.resolve(__dirname, '../src'),
+    // 入口配置
     entry: './main.js',
+    // 输入配置
     output: {
         path: path.resolve(__dirname, '../dist'),
+        // 文件名前面挂上hash
         filename: '[chunkhash].bundle.js'
+    },
+    devServer: {
+        // 给server提供内容的目录
+        contentBase: path.resolve(__dirname, '../dist'),
+        // 启动gzip压缩
+        compress: true,
+        // 端口号
+        port: 9000
     },
     // loaders
     module: {
@@ -23,8 +34,10 @@ module.exports = {
     // plugins
     plugins: [
         new htmlWebpackPlugin({
+            // 页面title
             title: 'learning webpack',
-            filename: 'index.html'
+            // 指定模版
+            template: 'index.html'
         })
     ]
 };
